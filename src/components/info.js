6a14bd06bcd638e3b11fb4726905d02d771752a2
Serhii Sheyko <serjioblack@gmail.com>
2024-02-26 06:49:33 +0200

    first commit

---
+++ src/components/info.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Info = (props) => (
+    <div>
+        <h2>Weather App</h2>
+        <p>Get weather forecast for your city</p>
+    </div>
+)
+  
+  export default Info;
